67f90962bd09e675c3605066649b8475aa48b907
jedifathan <m.jundi20@gmail.com>
2023-05-24 14:52:17 +0000

    implement jwt

--- src/main.py
+++ src/main.py
@@ -1,31 +1,19 @@
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Depends, Request, HTTPException, Response
 from fastapi.staticfiles import StaticFiles
 from schemas import CreateUser
 from core.crud import create_user
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
+from core.crud import crud
 from fastapi.templating import Jinja2Templates
+from django.http import HttpResponse
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="web")
 
 app.mount("/static", StaticFiles(directory="web/static"), name="static")
-
-# @app.post("/")
-# def create(details: CreateUser, db : Session = Depends(get_db)):
-#         to_create = User(
-#             username=details.username,
-#             password=details.password,
-#             report_bug=details.report_bug
-#         )
-#         db.add(to_create)
-#         db.commit()
-#         return{
-#             "success": True,
-#             "created_id": to_create.id
-#         }
     
 @app.get("/")
 def index(request: Request):
@@ -39,9 +27,12 @@ async def register(request: Request, db: Session = Depends(get_db)):
     username = form.get("username")
     password = form.get("password")
     cpassword = form.get("cpassword")
+    if password != cpassword:
+        raise HTTPException(status_code=400, detail="Passwords do not match")
+        return
     new_user = CreateUser(username=username, password=password)
     create_user(db, new_user)
-    return {"status": "OK"}
+    return templates.TemplateResponse("landing.html", context)
 
 @app.post("/login")
 async def register(request: Request, db: Session = Depends(get_db)):
@@ -49,15 +40,17 @@ async def register(request: Request, db: Session = Depends(get_db)):
     form = await request.form()
     username = form.get("username")
     password = form.get("password")
-    #print(username, password)
-    return {"status": "OK"}
+    token = crud.login_user(db, username, password)
+    response = HttpResponse()
+    response.set_cookie("token", token, max_age=24 * 60 * 60)
+    return templates.TemplateResponse("landing.html", context)
     
-
-# @app.delete("/")
-# def delete(id: int, db:Session=Depends(get_db)):
-#     return db.query(User).filter(User.id==id).delete()
-#     db.commit()
-#     return {"success": True}
+@app.get("/logout")
+async def logout(request: Request):
+    response = HttpResponse()
+    token = request.COOKIES.get("token")
+    response.delete_cookie("token")
+    return {"status": "OK"}
 
 if __name__ == "__main__":
     import uvicorn
