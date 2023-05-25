38ef3760a028aa172f4050e38efa8ba1f7d6c407
jedifathan <m.jundi20@gmail.com>
2023-05-25 09:16:54 +0000

    implement jwt (2)

--- src/core/crud.py
+++ src/core/crud.py
@@ -2,22 +2,33 @@ from sqlalchemy.orm import session
 from fastapi import HTTPException
 import schemas
 import models
-from auth import AuthHandler
+from core.auth import auth_handler
 
-class crud():
-    def create_user(db: session, new_user:schemas.CreateUser):
-        hashed_password = AuthHandler.get_password_hash(new_user.password)
+class Crud():
+    def create_user(self, db: session, new_user:schemas.CreateUser):
+        hashed_password = auth_handler.get_password_hash(new_user.password)
         user = models.User(username=new_user.username, password=hashed_password)
         db.add(user)
         db.commit()
 
-    def get_user(db: session, username: str):
+    def get_user(self, db: session, username: str):
         user = db.query(models.User).filter(models.User.username == username).first()
         return user
     
-    def login_user(db: session, username: str, password: str):
-        user = get_user(db, username)
-        if (user is None) or (not AuthHandler.verify_password(AuthHandler.password, user['password'])):
+    
+    def login_user(self, db: session, username: str, password: str):
+        user = self.get_user(db, username)
+        # print(user)
+        # print(user.password)
+        # print(auth_handler.get_password_hash(password))
+        # print(auth_handler.verify_password(user.password, auth_handler.get_password_hash(password)))
+        if (user is None) or (not auth_handler.verify_password(password, user.password)):
             raise HTTPException(status_code=401, detail='Invalid username and/or password')
-        token = AuthHandler.encode_token(user['username'])
+        token = auth_handler.encode_token(user.username)
         return { 'token': token }
+    
+    def verify_user(self, token: str):
+        user = auth_handler.decode_token(token)
+        return user
+    
+crud = Crud()
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, Depends, Request, HTTPException, Response
+from fastapi import FastAPI, Depends, Request, HTTPException, Response, status
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from schemas import CreateUser
-from core.crud import create_user
+from core.crud import crud
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
 from core.crud import crud
 from fastapi.templating import Jinja2Templates
-from django.http import HttpResponse
 
 app = FastAPI()
 
@@ -18,6 +18,11 @@ app.mount("/static", StaticFiles(directory="web/static"), name="static")
 @app.get("/")
 def index(request: Request):
     context = {"request": request}
+    token = request.cookies.get('token')
+    if token == None:
+        return templates.TemplateResponse("menu.html", context)
+    if crud.verify_user(token):
+        return templates.TemplateResponse("landing.html", context)
     return templates.TemplateResponse("menu.html", context)
 
 @app.post("/register")
@@ -31,28 +36,28 @@ async def register(request: Request, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Passwords do not match")
         return
     new_user = CreateUser(username=username, password=password)
-    create_user(db, new_user)
+    crud.create_user(db, new_user)
     return templates.TemplateResponse("landing.html", context)
 
 @app.post("/login")
-async def register(request: Request, db: Session = Depends(get_db)):
+async def login(request: Request, db: Session = Depends(get_db)):
     context = {"request": request}
     form = await request.form()
     username = form.get("username")
     password = form.get("password")
     token = crud.login_user(db, username, password)
-    response = HttpResponse()
-    response.set_cookie("token", token, max_age=24 * 60 * 60)
-    return templates.TemplateResponse("landing.html", context)
+    response = RedirectResponse("/", status_code=status.HTTP_302_FOUND)
+    response.set_cookie("token", token["token"], max_age=24 * 60 * 60)
+    return response
     
 @app.get("/logout")
 async def logout(request: Request):
-    response = HttpResponse()
-    token = request.COOKIES.get("token")
+    token = request.cookies.get("token")
+    response = RedirectResponse("/", status_code=status.HTTP_302_FOUND)
     response.delete_cookie("token")
-    return {"status": "OK"}
+    return response
 
-if __name__ == "__main__":
-    import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
